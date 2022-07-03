668ebb6e84b275260c9cbc291c3b2bdde3cb2e71
Shayan Shojaei <shayanshojaei.2002@gmail.com>
2022-07-03 22:09:00 +0430

    fix: change colors

--- src/components/main/footer/Footer.tsx
+++ src/components/main/footer/Footer.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 
 import styles from '@/styles/main/footer.module.scss';
 
-import Socials from '../socials/Socials';
-
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Socials />
-      <h4>© 2022 Nott Platform, All Rights Reserved.</h4>
+      <h4>© 2022 Insight.d, All Rights Reserved.</h4>
     </footer>
   );
 }
